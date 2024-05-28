ce07d040421c3ffecb0fc2e04c481abca15a585e
Xuo1z <149414772+Xuo1z@users.noreply.github.com>
2024-05-28 15:14:36 +0300

    Экспорт данных

---
+++ controllers/categories.js
@@ -0,0 +1,26 @@
+const sendAllCategories = (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(req.categoriesArray));
+};
+
+const sendCategoryCreated = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(req.category));
+};
+
+const sendCategoryDeleted = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(req.category));
+};
+
+const sendCategoryUpdated = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end("Категория  обновлена!");
+};
+
+module.exports = {
+  sendAllCategories,
+   sendCategoryCreated,
+   sendCategoryDeleted,
+  sendCategoryUpdated,
+ };
